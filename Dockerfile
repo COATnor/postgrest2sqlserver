771e01f167db802fab4dc171864061c6178a33bb
Francesco Frassinelli <francesco.frassinelli@nina.no>
2023-03-29 15:52:16 +0200

    Adapt datetime with timezone to SQL Server

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM mcr.microsoft.com/mssql-tools
 
-RUN apt-get update && apt-get install -qy --no-install-recommends miller
+RUN apt-get update && apt-get install -qy --no-install-recommends miller gawk
 COPY --chmod=+x move_data.sh /usr/local/bin/move_data.sh
 
 ENV DELETE=false
--- move_data.sh
+++ move_data.sh
@@ -11,7 +11,8 @@ curl --fail --silent \
     "$URL?$PK_NAME=lte.$pk" \
     --header "Authorization: Bearer $CONSUMER_TOKEN" \
     --header "Accept: application/json" |
-     mlr --ijson --ocsv --ofs tab --quote-none cat > exported.tsv
+     mlr --ijson --ocsv --ofs tab --quote-none cat |
+    awk -F$'\t' '{OFS=FS} { for (i=1; i<=NF; i++) $i=gensub(/^([0-9]{4}-[0-9]{2}-[0-9]{2})T([0-9]{2}:[0-9]{2}:[0-9]{2})([+-][0-9]{2}:[0-9]{2})$/, "\\1 \\2 \\3", "g", $i); print}' > exported.tsv
 
 "$@"
 
