ca1eea6b89b03428c313ffd9d1c4ee9461fe7fcc
Francesco Frassinelli <francesco.frassinelli@nina.no>
2023-03-29 17:44:09 +0200

    Add comments and print messages to stderr

--- move_data.sh
+++ move_data.sh
@@ -2,6 +2,8 @@
 
 set -Eeuo pipefail
 
+# Get the last PK
+>&2 echo "Fetching the greatest primary key..."
 pk="$(curl --fail --silent \
     "$URL?select=$PK_NAME&order=$PK_NAME.desc&limit=1" \
     --header "Authorization: Bearer $CONSUMER_TOKEN" \
@@ -10,9 +12,12 @@ pk="$(curl --fail --silent \
 # Empty table
 if [ ! -n "$pk" ]
 then
+    >&2 echo "The table is empty."
     exit
 fi
 
+# Fetch the data and convert it
+>&2 echo "Fetching the data..."
 curl --fail --silent \
     "$URL?$PK_NAME=lte.$pk" \
     --header "Authorization: Bearer $CONSUMER_TOKEN" \
@@ -20,11 +25,17 @@ curl --fail --silent \
      mlr --ijson --ocsv --ofs tab --quote-none cat |
     awk -F$'\t' '{OFS=FS} { for (i=1; i<=NF; i++) $i=gensub(/^([0-9]{4}-[0-9]{2}-[0-9]{2})T([0-9]{2}:[0-9]{2}:[0-9]{2})([+-][0-9]{2}:[0-9]{2})$/, "\\1 \\2 \\3", "g", $i); print}' > exported.tsv
 
+# Run the command
+>&2 echo "Executing the program..."
 "$@"
+>&2 echo "Program executed successfully."
 
+# Delete (optional)
 if [ "$DELETE" = true ]
 then
+    >&2 echo "Deleting the original data..."
     curl --fail --silent --request DELETE \
         "$URL?$PK_NAME=lte.$pk" \
         --header "Authorization: Bearer $CONSUMER_TOKEN"
+    >&2 echo "Data deleted."
 fi
